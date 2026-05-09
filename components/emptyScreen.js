54e07e4073f34bd08dd7d3d4e9fdd6e4621fb599
Samuel Ramirez <samuel_ramirez@live.com.mx>
2026-05-08 19:22:21 -0600

    Empty screen (#1)
    * add image on empty screen
    
    * preserve zoom

---
+++ components/emptyScreen.js
@@ -0,0 +1,10 @@
+export const EmptyScreen = `
+  <div class="empty-screen">
+    <img
+      src="/icons/bible-big.png"
+      id="empty-screen-image"
+      class="empty-screen__image"
+      draggable="false"
+    />
+  </div>
+`;
